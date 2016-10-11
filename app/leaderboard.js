d93af0bd35145da015afc8d3b9bd04e36f365a13
cpalpall <cpalpall@hawaii.edu>
2016-10-11 10:16:02 -1000

    Task 5: Events, Sessions

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -1,1 +1,32 @@
-console.log("Hello world");
+
+PlayersList = new Mongo.Collection('players');
+
+if(Meteor.isClient){
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find();
+    },
+    'otherHelperFunction': function(){
+
+    },
+    'selectedClass': function() {
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer')
+      if (playerId == selectedPlayer) {
+        return "selected"
+      }
+    }
+  });
+  Template.leaderboard.events({
+    'click .player': function() {
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+      var selectedPlayer = Session.get('selectedPlayer');
+      console.log(selectedPlayer);
+    }
+  });
+}
+
+if(Meteor.isServer){
+
+}
